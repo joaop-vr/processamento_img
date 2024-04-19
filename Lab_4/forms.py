da1da9cf304e669c1e5f0cab64d9e6d6e19f2bb9
joaop-vr <jpv.ramalho2013@gmail.com>
2024-04-19 01:58:12 -0300

    [add] resposta das perguntas binárias esta correta

--- Lab_4/forms.py
+++ Lab_4/forms.py
@@ -2,9 +2,9 @@ import sys
 import os
 import cv2
 import numpy as np
+from math import floor
 import matplotlib.pyplot as plt
 
-rois = []
 
 
 """# Variáveis delimitadoras da região de interesse (ROI)
@@ -57,7 +57,7 @@ h = 220
 cv2.rectangle(img, (x,y), (x+w, y+h), (255,0, 255), 2)"""
 
 
-def segment_form(img_path):
+def segment_form(img_path, rois):
 
     # Carregar imagem
     img = cv2.imread(img_path)
@@ -75,13 +75,40 @@ def segment_form(img_path):
         filename = "roi"+str(i) +".png"
         cv2.imwrite(filename, roi)
 
-
         img_segmented = cv2.imread(filename)
         # Calcular o histograma ao longo do eixo X
         histogram = np.sum(img_segmented, axis=0)
 
         # Normalizar o histograma
         histogram = histogram / np.max(histogram)
+
+        if 6 <= i <= 8:
+            middle_index = np.floor(len(histogram) / 2).astype(int)
+            
+            # Encontrar o índice do menor valor do histograma normalizado
+            #min_magnitude_index = np.abs(histogram[2:-2]).argmin()
+    
+            # Retorna o valor do menor magnitude e o índice
+            #min_magnitude_value = histogram[min_magnitude_index]
+
+            # Extrai o campo central de cada tupla (índice 1)
+            central_values = [tup[1] for tup in histogram[2:-2]]
+            
+            # Calcula os valores absolutos
+            absolute_values = np.abs(central_values)
+            
+            # Encontra o índice do menor valor em magnitude
+            min_magnitude_index = absolute_values.argmin()
+
+            #for i in range(len(histogram)):
+                #print(f"histogram[{i}]: {histogram[i]}")
+
+            #print(f"middle: {middle_index} | min_index: {min_magnitude_index} | min_value: {central_values[min_magnitude_index]}")
+
+            if min_magnitude_index <= middle_index:
+                print(filename + " -> Yes\n")
+            else:
+                print(filename + " -> No\n")
 
         # Plotar
         plt.figure(figsize=(10, 5))
@@ -103,23 +130,36 @@ def segment_form(img_path):
 
 def define_rois():
 
+    rois = []
+
     # Coordenadas pré estabelecidas para as áreas de interesse (ROI)
-    roi_1 = (850, 660, 1560, 1110)
+    roi_1 = (850, 760, 1560, 120)
+    roi_11 = (850, 950, 1560, 110)
+    roi_12 = (850, 1120, 1560, 120)
+    roi_13 = (850, 1310, 1560, 120)
+    roi_14 = (850, 1490, 1560, 120)
+    roi_15 = (850, 1670, 1560, 90)
     roi_2 = (850, 1770, 570, 200)
     roi_3 = (70, 2070, 600, 250)
     roi_4 = (1330, 2070, 700, 220)
     roi_5 = (900, 2290, 1480, 220)
 
     # Adiciona os rois à lista global
-    global rois
     rois.append(roi_1)
+    rois.append(roi_11)
+    rois.append(roi_12)
+    rois.append(roi_13)
+    rois.append(roi_14)
+    rois.append(roi_15)
     rois.append(roi_2)
     rois.append(roi_3)
     rois.append(roi_4)
     rois.append(roi_5)
 
+    return rois
 
-def main(input_dir, output_dir=None):
+
+def main(rois, input_dir, output_dir=None):
     
     # Verifica se o diretório de saída existe e criar se não existe
     if output_dir and not os.path.exists(output_dir):
@@ -133,7 +173,7 @@ def main(input_dir, output_dir=None):
         img_path = os.path.join(input_dir, arquivo)
 
         # Segmentar os formulários
-        segment_form(img_path)
+        segment_form(img_path, rois)
 
     
 
@@ -149,5 +189,5 @@ if __name__ == "__main__":
     input_dir = sys.argv[1]
     output_dir = sys.argv[2] if len(sys.argv) > 2 else None
 
-    define_rois()
-    main(input_dir, output_dir)
+    rois = define_rois()
+    main(rois, input_dir, output_dir)
